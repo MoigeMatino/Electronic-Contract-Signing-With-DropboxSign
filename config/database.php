7badee19866c530d3142d74aeaa1c8ae114244d9
Lewis Munyi <lewismunyiii@gmail.com>
2024-03-12 23:49:44 +0300

    Bug fix: DB connections

--- config/database.php
+++ config/database.php
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => env('MYSQL_DATABASE_URL'),
+            'host' => env('MYSQL_DB_HOST', '127.0.0.1'),
+            'port' => env('MYSQL_DB_PORT', '3306'),
+            'database' => env('MYSQL_DB_DATABASE', 'forge'),
+            'username' => env('MYSQL_DB_USERNAME', 'forge'),
+            'password' => env('MYSQL_DB_PASSWORD', ''),
+            'unix_socket' => env('MYSQL_DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('PGSQL_DATABASE_URL'),
+            'host' => env('PGSQL_DB_HOST', '127.0.0.1'),
+            'port' => env('PGSQL_DB_PORT', '5432'),
+            'database' => env('PGSQL_DB_DATABASE', 'forge'),
+            'username' => env('PGSQL_DB_USERNAME', 'forge'),
+            'password' => env('PGSQL_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -80,12 +80,12 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('SQLSRV_DATABASE_URL'),
+            'host' => env('SQLSRV_DB_HOST', 'localhost'),
+            'port' => env('SQLSRV_DB_PORT', '1433'),
+            'database' => env('SQLSRV_DB_DATABASE', 'forge'),
+            'username' => env('SQLSRV_DB_USERNAME', 'forge'),
+            'password' => env('SQLSRV_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
